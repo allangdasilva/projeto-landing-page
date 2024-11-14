19fc2a888a62df1660c6356615d0ea6cf3e51655
Allan Silva <allandasilva33@outlook.com>
2024-11-14 01:32:35 -0300

    Estilização da section 03 (mobile e desktop) completa

--- js/main.js
+++ js/main.js
@@ -7,3 +7,31 @@ function thumbClose(){
     VIDEO_THUMBNAIL.classList.add('video-thumbnail-active');
 }
 VIDEO_THUMBNAIL.addEventListener('click', thumbClose);
+
+//Slider Section 03
+const SLIDER_SEC_03 = document.querySelector('.section-03-slider');
+
+new Glider(SLIDER_SEC_03, {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    scrollLock: true,
+    arrows: {
+      prev: '.section-03-slider-prev-button',
+      next: '.section-03-slider-next-button'
+    },
+    responsive: [
+        {
+          breakpoint: 550,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 3
+            }
+          }
+    ],
+  });
